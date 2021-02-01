9b3425a7ed806726ddedbe6b3128c6122378777d
studiocliq <parkgone23@gmail.com>
2021-02-01 10:21:10 +0900

    fix

--- js/height-setter.js
+++ js/height-setter.js
@@ -1,31 +1,6 @@
-var standardElement = $('#weekly-container');
-var standardHeight = standardElement.clientHeight;
+var juhunHeight = $('#juhun_description').height();
+console.log($('#juhun-description'))
 
-// Set height of other box
-var daily_container = $("#daily-height-setter");
-var monthly_container = $("#monthly-height-setter")
+$('#jiwon_description').height(juhunHeight);
 
-// monthly_container.style.height = standardElement.clientWidth * 1.393;
-// daily_container.style.height = standardElement.clientWidth * 1.393;
-
-daily_container.height(standardElement.height());
-monthly_container.height(standardElement.height());
-
-console.log(monthly_container.height());
-
-
-function PrintDiv(div){
-	div = div[0]
-	html2canvas(div).then(function(canvas){
-		var myImage = canvas.toDataURL();
-		downloadURI(myImage, "저장이미지이름.png") 
-	});
-}
-
-function downloadURI(uri, name){
-	var link = document.createElement("a")
-	link.download = name;
-	link.href = uri;
-	document.body.appendChild(link);
-	link.click();
-}
+console.log(juhunHeight);
